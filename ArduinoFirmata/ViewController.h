85216996c2e96b4cf63184a8fa3d8c390ba96a33
Maximilian Ebert <maximilianebert@Maximilians-MacBook-Pro.local>
2015-04-11 22:10:18 -0400

    Initial Commit

---
+++ ArduinoFirmata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArduinoFirmata
+//
+//  Created by Maximilian Ebert on 2015-04-11.
+//  Copyright (c) 2015 Maximilian Ebert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
